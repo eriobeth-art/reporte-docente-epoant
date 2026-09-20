56a39062b8c87638446a684e0fc5e306ffdf2cdf
eriobeth-art <eriobeth@gmail.com>
2026-09-19 18:12:11 -0600

    Delete config.js

--- config.js
+++
@@ -1,1 +0,0 @@
-window.REPORTE_DOCENTE_CONFIG={APP_URL:"https://script.google.com/macros/s/AKfycbxPCoy05SdDLf9B7B9CsZx9ICDV6pqX9w4wEX19NZ7I1fuw2eAo74MiRZ3yCwjY357E/exec",DOMAIN:"reportedocente.edupsic.com"};
